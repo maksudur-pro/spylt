7d38ef3639aae446ae6a790522846a33b5e767ef
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-11-08 15:33:07 +0600

    added navbar section

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <nav className="fixed top-0 left-0 z-50 md:p-9 lg:p-9 p-3">
+      <img
+        src="src/assets/images/nav-logo.svg"
+        alt="nav-logo"
+        className="lg:w-24 md:w-24 w-20"
+      />
+    </nav>
+  );
+};
+
+export default NavBar;
